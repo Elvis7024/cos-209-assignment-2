e9bf7d0a4e112aaf70987c58f686f6421e691de3
Elvis7024 <elvisnwangwu2008@gmail.com>
2025-10-04 20:25:05 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import About from "./about";
+import Header from "./header";
+import Footer from "./Footer";
+import Food from "./Food";
+function App() {
+  
+  return(
+    <>
+        <About/>
+        <Header/>
+         <Food/>
+        <Footer/>   
+    </>
+);
+}
+
+export default App
---
+++ about.jsx
@@ -0,0 +1,7 @@
+
+function about(){
+    return(
+        <h1>THIS IS THE ABOUT SECTION</h1>
+    );
+}
+export default about
